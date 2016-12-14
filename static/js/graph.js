0e7774e74b3696ce30507dbf44df24520f66bb3b
Paul Kane <kanepa1977@gmail.com>
2016-12-14 21:50:48 +0000

    put z back

--- static/js/graph.js
+++ static/js/graph.js
@@ -10,7 +10,6 @@ function makeGraphs(error, celticJson) {
         p["Career Length"] = p["Career Finish"] - p["Career Start"];
         p["Goals Per Game"] = Math.round(p["Goals"]/p["Appearances"], 3);
 
-
         if (!p["Career Length"]) p["Career Length"] = 0;
         console.log(p["Career Length"]);
     });
@@ -153,8 +152,7 @@ var dim_goals_per_game;
                 .height(250)
                 .dimension(resourceTypeDim)
                 .group(numrangers)
-                .xAxis().ticks(4)
-                .elasticX(true);
+                .xAxis().ticks(4);
 
 
 
